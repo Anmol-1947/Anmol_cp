283d7cbefd7d9439d745717d808c3d5a42f3e061
Anmol-1947 <anmolghariwala19@gmail.com>
2025-08-26 01:24:47 +0530

    Add files via upload

---
+++ anmolcp5.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,add,sub,prod,divi;
+	printf("Enter the first number ");
+	scanf("%d",&a);
+	printf("Enter the second number ");
+	scanf("%d",&b);
+
+	add=a+b;
+	printf("The sum of two numbers is = %d" \n ,add);
+
+	prod=a*b;
+	printf("The product of two numbers is = %d \n" ,prod);
+
+	c=a-b;
+	printf("The difference of two numbers is = %d \n",sub);
+	
+	c=a/b;
+	printf("The quotient of two numbers is = %d \n",divi);
+	
+
+}
